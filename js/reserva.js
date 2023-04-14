d3c73c19d1093f794a86218af2a6cc534d9168d8
Ezequiel Villalba <60276551+therzer@users.noreply.github.com>
2023-04-14 18:00:53 -0300

    tooltip
    inserte tooltip de bootstrap en los input del formulario

--- js/reserva.js
+++ js/reserva.js
@@ -1,3 +1,9 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="mensaje"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl){
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
 $(".tab").css("display","none");
 $("#tab-1").css("display","block");
 

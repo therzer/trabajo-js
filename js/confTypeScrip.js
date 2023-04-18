00a106b8bb62e1ebbe829a8ed4317691c66ade8c
Ezequiel Villalba <60276551+therzer@users.noreply.github.com>
2023-04-17 21:59:35 -0300

    efecto escritura titulo principal

---
+++ js/confTypeScrip.js
@@ -0,0 +1,29 @@
+var typed = new Typed('.typed', {
+	stringsElement: '#repetidor', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 80, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '.', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+var typed = new Typed('.parrafo', {
+	stringsElement: '#parrafo', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 20, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '.', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});

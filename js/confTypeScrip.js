e51b0afb39d114ef2d629852aa13f00884148aaf
Lucas Basilio <lucasebasilio@outlook.com>
2023-04-18 12:36:25 -0300

    a

--- js/confTypeScrip.js
+++ js/confTypeScrip.js
@@ -15,9 +15,9 @@ var typed = new Typed('.typed', {
 
 var typed = new Typed('.parrafo', {
 	stringsElement: '#parrafo', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 20, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 15, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
+	backSpeed: 80, // Velocidad en milisegundos para borrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 2000, // Tiempo de espera despues de que termina de escribir una palabra.
